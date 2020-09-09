0f1d9e37d2e1c40d48ade301c17642a9b260566b
Zhengyang Cui <taberareru1989@gmail.com>
2020-09-08 20:46:11 -0700

    initial commit

---
+++ app/src/main/java/com/example/tinderclone/activities/TinderCallback.kt
@@ -0,0 +1,13 @@
+package com.example.tinderclone.activities
+
+import com.google.firebase.database.DatabaseReference
+
+interface TinderCallback {
+
+    fun onSignout()
+    fun onGetUserId(): String
+    fun getUserDatabase(): DatabaseReference
+    fun getChatDatabase(): DatabaseReference
+    fun profileComplete()
+    fun startActivityForPhoto()
+}
